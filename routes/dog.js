78de06d218646ae9a22824019d8a693238cb5c1f
hayataka04 <165109312+hayataka04@users.noreply.github.com>
2024-08-08 10:50:04 +0900

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://qiita.com/miriwo/items/c1727868d9225f4e4c4a', function (error, response,
+request('https://dog.ceo/api/breeds/image/random', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
